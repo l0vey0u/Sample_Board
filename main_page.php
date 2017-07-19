1d0c9d5a69b8c1b74bcc7dcdb772687909acdbba
duck5 <duckhoim@kyonggi.ac.kr>
2017-07-19 16:02:53 +0900

    Basic_Step : frame arrangement in Login Form
    <To Do>
    - Positioning
    - Resizing
    - and so on

--- main_page.php
+++ main_page.php
@@ -5,6 +5,14 @@
 	<title> Sample_Board </title>
 </head>
 <body>
+	<style type="text/css">
+		.btn {
+			display: block;
+			padding: 10px;
+			border: 0.5px solid black;
+			text-align: center;
+		}
+	</style>
 	<!-- LOGO -->
 	<img id=logo src="http://placehold.it/384x712"/>
 	<style type="text/css">
@@ -18,6 +26,51 @@
 
 	<!-- Login Form -->
 	<form id=login_form action="#" method="post">
+		<input type="text" placeholder="ID"/>
+		<input type="password" placeholder="PW"/>
+		<input type="submit" value="login"/>
+		<style type="text/css">
+			input[type="text"], input[type="password"] {
+				border: 0 solid black;
+				border-bottom: 1px solid black;
+				width: 80%;
+				height: 20%;
+				text-align: center;
+			}
+			input[type="submit"] {
+				border: 0 solid black;
+				background: rgba(0,0,0,0);
+				width: 50%;
+				height: 10%;
+				text-align: center;
+			}
+			input[type="text"] {
+				position: absolute;
+				top: 10%;
+				bottom: 70%;
+			}
+			input[type="password"] {
+				position: absolute;
+				top: 35%;
+				bottom: 45%;
+			}
+			input[type="submit"] {
+				position: absolute;
+				top: 60%;
+				bottom: 30%;
+			}
+		</style>
+		<a herf="#" id=Register class="btn"> JOIN </a>
+		<style type="text/css">
+			a[id=Register] {
+				position: absolute;
+				top: 85%;
+				bottom: 0;
+				width: 80%;
+				height: 15%;
+				text-align: center;
+			}
+		</style>
 	</form>
 	<style type="text/css">
 		#login_form {
@@ -35,18 +88,13 @@
 	<a herf="#" id=view class="btn"> View </a>
 	<a herf="#" id=write class="btn"> Write </a>
 	<style type="text/css">
-		.btn {
-			display: block;
-			width: 384px;
-			height: 142px;
-			padding: 10px;
-			border: 0.5px solid black;
-			text-align: center;
-		}
 		#view, #write {
 			position: absolute;
 			left:70%;
+			width: 384px;
+			height: 142px;
 			line-height: 142px;
+
 		}
 		#view {
 			top: 10%;
@@ -59,4 +107,3 @@
 	</style>
 </body>
 </html>
-
