6e44adb7708a887a2944b7aaefa098b73d6fe6b6
duck5 <duckhoim@kyonggi.ac.kr>
2017-07-22 23:27:31 +0900

    implement login , divided css code into .css

--- main_page.php
+++ main_page.php
@@ -1,109 +1,37 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+session_start();
+$_SESSION['is_login'];
+if($_SESSION['is_login'] != TRUE and $_SESSION['is_login'] != FALSE ) {
+	$_SESSION['is_login'] = FALSE;
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<title> Sample_Board </title>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
 <body>
-	<style type="text/css">
-		.btn {
-			display: block;
-			padding: 10px;
-			border: 0.5px solid black;
-			text-align: center;
-		}
-	</style>
 	<!-- LOGO -->
-	<img id=logo src="http://placehold.it/384x712"/>
-	<style type="text/css">
-		#logo {
-			position: absolute;
-			top:10%;
-			left:10%;
-			bottom: 15%;
-		}
-	</style>
-
-	<!-- Login Form -->
-	<form id=login_form action="#" method="post">
-		<input type="text" placeholder="ID"/>
-		<input type="password" placeholder="PW"/>
+	<img id=logo src="http://placehold.it/384x712"/> 
+	<?php 
+	if (!$_SESSION['is_login']) {
+		/* Login Form */
+	 echo '<form id=login_form action="login_check.php" method="post">
+		<input name="id" type="text" placeholder="ID"/>
+		<input name="pw" type="password" placeholder="PW"/>
 		<input type="submit" value="login"/>
-		<style type="text/css">
-			input[type="text"], input[type="password"] {
-				border: 0 solid black;
-				border-bottom: 1px solid black;
-				width: 80%;
-				height: 20%;
-				text-align: center;
-			}
-			input[type="submit"] {
-				border: 0 solid black;
-				background: rgba(0,0,0,0);
-				width: 50%;
-				height: 10%;
-				text-align: center;
-			}
-			input[type="text"] {
-				position: absolute;
-				top: 10%;
-				bottom: 70%;
-			}
-			input[type="password"] {
-				position: absolute;
-				top: 35%;
-				bottom: 45%;
-			}
-			input[type="submit"] {
-				position: absolute;
-				top: 60%;
-				bottom: 30%;
-			}
-		</style>
-		<a herf="#" id=Register class="btn"> JOIN </a>
-		<style type="text/css">
-			a[id=Register] {
-				position: absolute;
-				top: 85%;
-				bottom: 0;
-				width: 80%;
-				height: 15%;
-				text-align: center;
-			}
-		</style>
-	</form>
-	<style type="text/css">
-		#login_form {
-			position: absolute;
-			top:10%;
-			left:40%;
-			bottom: 15%;
-			border: 0.5px solid black;
-			width: 384px;
-			height: 712px;
-		}
-	</style>
-
+		<a href="signup.php" id=Register class="btn"> JOIN </a>
+		</form>';
+	} else {
+		echo $_SESSION['id']." 안뇽";
+	}
+?>
 	<!-- Action -->
-	<a herf="#" id=view class="btn"> View </a>
-	<a herf="#" id=write class="btn"> Write </a>
-	<style type="text/css">
-		#view, #write {
-			position: absolute;
-			left:70%;
-			width: 384px;
-			height: 142px;
-			line-height: 142px;
-
-		}
-		#view {
-			top: 10%;
-			bottom: 75%;
-		}
-		#write {
-			top: 40%;
-			bottom: 45%;
-		}
-	</style>
+	<a href="#" id=view class="btn"> View </a>
+	<a href="#" id=write class="btn"> Write </a>
 </body>
 </html>
